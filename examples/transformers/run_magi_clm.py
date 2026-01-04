b5aee13c2aa7ce64ec6518087d37e05e10abe81f
Yunpeng Huang <hyp@smail.nju.edu.cn>
2026-01-04 14:08:39 +0800

    Update copyright for 2026 (#203)
    * updated copyright from 2025 to 2026
    
    * fixed workflow for MagiAttentionCsrc outputs

--- examples/transformers/run_magi_clm.py
+++ examples/transformers/run_magi_clm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# Copyright (c) 2025 SandAI. All Rights Reserved.
+# Copyright (c) 2025-2026 SandAI. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
